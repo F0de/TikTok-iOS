2f2cf10166c542ecc6b0165c356c7f843385c753
F0de <78221051+F0de@users.noreply.github.com>
2024-06-30 22:35:26 +0300

    Initial Commit

---
+++ TikTok/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TikTok
+//
+//  Created by Влад Тимчук on 30.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
